00d532277496b627c5b901b04af7b2a8d4095da8
GitLabSJ <aeeou12@gmail.com>
2023-04-27 00:39:56 +0900

    feat: styles 세팅

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
+  bgColor: "#0223FF",
+  textColor: "#FE23FF",
+  accentColor: "#27FD1C",
 };
