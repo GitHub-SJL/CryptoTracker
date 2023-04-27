853c3cb29b659e116e24f155c33881b4418bd3f3
GitLabSJ <aeeou12@gmail.com>
2023-04-27 11:40:26 +0900

    feat: coin데이터 API interface 생성

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -31,12 +31,130 @@ interface RouteState {
   name: string;
 }
 
+interface IContract {
+  contract: string;
+  platform: string;
+  type: string;
+}
+
+interface ILinks {
+  explorer: string[];
+  facebook: string[];
+  reddit: string[];
+  website: string[];
+  youtube: string[];
+}
+
+interface ILinksExtended {
+  url: string;
+  type: string;
+  stats?: IStats;
+}
+
+interface IStats {
+  subscribers?: number;
+  followers?: number;
+}
+
+interface IParent {
+  id: string;
+  name: string;
+  symbol: string;
+}
+
+interface ITag {
+  id: string;
+  name: string;
+  coin_counter: number;
+  ico_counter: number;
+}
+
+interface ITeam {
+  id: string;
+  name: string;
+  position: string;
+}
+
+interface IWhitepaper {
+  link: null;
+  thumbnail: null;
+}
+
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  contract: string;
+  platform: string;
+  contracts: IContract[];
+  logo: string;
+  parent: IParent;
+  tags: ITag[];
+  team: ITeam[];
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: Date;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  links: ILinks;
+  links_extended: ILinksExtended[];
+  whitepaper: IWhitepaper;
+  first_data_at: Date;
+  last_data_at: Date;
+}
+
+interface IQuotes {
+  USD: IUsd;
+}
+
+interface IUsd {
+  price: number;
+  volume_24h: number;
+  volume_24h_change_24h: number;
+  market_cap: number;
+  market_cap_change_24h: number;
+  percent_change_15m: number;
+  percent_change_30m: number;
+  percent_change_1h: number;
+  percent_change_6h: number;
+  percent_change_12h: number;
+  percent_change_24h: number;
+  percent_change_7d: number;
+  percent_change_30d: number;
+  percent_change_1y: number;
+  ath_price: number;
+  ath_date: Date;
+  percent_from_price_ath: number;
+}
+
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: Date;
+  last_updated: Date;
+  quotes: IQuotes;
+}
+
 const Coin = () => {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouterParams>();
   const { state } = useLocation<RouteState>();
-  const [info, setInfo] = useState({});
-  const [priceInfo, setPriceInfo] = useState({});
+  const [info, setInfo] = useState<InfoData | undefined>(undefined);
+  const [priceInfo, setPriceInfo] = useState<PriceData | undefined>(undefined);
 
   useEffect(() => {
     axios.get(`https://api.coinpaprika.com/v1/coins/${coinId}`).then((data) => {
@@ -50,8 +168,8 @@ const Coin = () => {
       });
   }, []);
 
-  console.log(info);
-  console.log(priceInfo);
+  console.log(info, "info");
+  console.log(priceInfo, "priceInfo");
   return (
     <Container>
       <Header>
